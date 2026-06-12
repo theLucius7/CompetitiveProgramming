1fa7640858b13b4cd86ae60bdfe4ce8c71ca8336
theLucius7 <lucius7nya@gmail.com>
2026-06-12 22:11:32 +0800

    qwq and ovo and o0o

--- =qwqwqwq=/codeforces/2233/D_Goods_on_the_Shelf.cpp
+++ =qwqwqwq=/codeforces/2233/D_Goods_on_the_Shelf.cpp
@@ -17,66 +17,73 @@ void solve() {
     }
 
     int cnt = 0;
-    std::vector<int> v;
     for (int i = 1; i < n; i++) {
         if (a[i] != a[i - 1]) {
             cnt++;
-            v.push_back(i - 1);
-            v.push_back(i);
         }
     }
 
-    v.erase(std::unique(v.begin(), v.end()), v.end());
+    std::map<int, std::vector<std::pair<int, int>>> mp;
+    for (int l = 0, r = 0; l < n; l = r) {
+        while (r < n && a[l] == a[r]) {
+            r++;
+        }
+        mp[a[l]].push_back({l, r - 1});
+    }
+
+    int mx = 0;
+    std::vector<int> vec;
+    for (auto [k, v] : mp) {
+        if (v.size() > 4) {
+            std::cout << "NO\n";
+            return;
+        }
+        
+        if (v.size() > mx) {
+            mx = v.size();
+            vec.clear();
+            for (int i = 0; i < v.size(); i++) {
+                vec.push_back(std::max(0LL, v[i].first - 1));
+                vec.push_back(v[i].first);
+                vec.push_back(v[i].second);
+                vec.push_back(std::min(n - 1, v[i].second + 1));
+            }
+        }
+
+    }
+
+    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
 
     if (cnt == s.size() - 1) {
         std::cout << "YES\n";
         return;
     }
 
-    if (cnt > s.size() + 4) {
-        std::cout << "NO\n";
-        return;
-    }
-    std::map<std::pair<int, int>, bool> vis;
-    for (auto x : v) {
+    for (auto x : vec) {
         for (int y = 0; y < n; y++) {
-            if (x == y || vis[{std::min(x, y), std::max(x, y)}]) {
+            if (x == y) {
                 continue;
             }
-
 
             int d = 0;
 
             if (x + 1 == y) {
-                d += (x - 1 >= 0) ? ((a[y] != a[x - 1]) - (a[x] != a[x - 1])) : 0;
-                d += (y + 1 < n) ? ((a[x] != a[y + 1]) - (a[y] != a[y + 1])) : 0;
+                d += x - 1 >= 0 ? (a[y] != a[x - 1]) - (a[x] != a[x - 1]) : 0;
+                d += y + 1 < n ? (a[x] != a[y + 1]) - (a[y] != a[y + 1]) : 0;
             } else if (y + 1 == x) {
-                d += (y - 1 >= 0) ? ((a[x] != a[y - 1]) - (a[y] != a[y - 1])) : 0;
-                d += (x + 1 < n) ? ((a[y] != a[x + 1]) - (a[x] != a[x + 1])) : 0;
+                d += y - 1 >= 0 ? (a[x] != a[y - 1]) - (a[y] != a[y - 1]) : 0;
+                d += x + 1 < n ? (a[y] != a[x + 1]) - (a[x] != a[x + 1]) : 0;
             } else {
-                if (x - 1 >= 0) {
-                    d += (a[y] != a[x - 1]) - (a[x] != a[x - 1]);
-                }
-                if (x + 1 < n) {
-                    d += (a[y] != a[x + 1]) - (a[x] != a[x + 1]);
-                }
-                if (y - 1 >= 0) {
-                    d += (a[x] != a[y - 1]) - (a[y] != a[y - 1]);
-                }
-                if (y + 1 < n) {
-                    d += (a[x] != a[y + 1]) - (a[y] != a[y + 1]);
-                }
+                d += x - 1 >= 0 ? (a[y] != a[x - 1]) - (a[x] != a[x - 1]) : 0;
+                d += x + 1 < n ? (a[y] != a[x + 1]) - (a[x] != a[x + 1]) : 0;
+                d += y - 1 >= 0 ? (a[x] != a[y - 1]) - (a[y] != a[y - 1]) : 0;
+                d += y + 1 < n ? (a[x] != a[y + 1]) - (a[y] != a[y + 1]) : 0;
             }
 
             if (cnt + d == s.size() - 1) {
-                // std::cout << cnt << / " " << d << " " << s.size() << "///";
-                // std::cout << x << " " << y << "\n";
                 std::cout << "YES\n";
-                
                 return;
             }
-
-            vis[{std::min(x, y), std::max(x, y)}] = true;
             
         }
     }
