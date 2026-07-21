657e5434dacf0f51726f62fe8783ef94cdffba13
theLucius7 <lucius7nya@gmail.com>
2026-07-21 20:51:11 +0800

    qwq and ovo and o0o

---
+++ =qwqwqwq=/codeforces/999/E_Reachability_from_the_Capital.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+struct SCC {
+    int n;
+    std::vector<std::vector<int>> adj;
+    std::vector<int> stk;
+    std::vector<int> dfn, low, bel;
+    int cur, cnt;
+    
+    SCC() {}
+    SCC(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        this->n = n;
+        adj.assign(n, {});
+        dfn.assign(n, -1);
+        low.resize(n);
+        bel.assign(n, -1);
+        stk.clear();
+        cur = cnt = 0;
+    }
+    
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+    
+    void dfs(int x) {
+        dfn[x] = low[x] = cur++;
+        stk.push_back(x);
+        
+        for (auto y : adj[x]) {
+            if (dfn[y] == -1) {
+                dfs(y);
+                low[x] = std::min(low[x], low[y]);
+            } else if (bel[y] == -1) {
+                low[x] = std::min(low[x], dfn[y]);
+            }
+        }
+        
+        if (dfn[x] == low[x]) {
+            int y;
+            do {
+                y = stk.back();
+                bel[y] = cnt;
+                stk.pop_back();
+            } while (y != x);
+            cnt++;
+        }
+    }
+    
+    std::vector<int> work() {
+        for (int i = 0; i < n; i++) {
+            if (dfn[i] == -1) {
+                dfs(i);
+            }
+        }
+        return bel;
+    }
+};
+
+void solve() {
+    int n, m, s;
+    std::cin >> n >> m >> s;
+    s--;
+
+    SCC g(n);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        std::cin >> u >> v;
+        u--, v--;
+        g.addEdge(u, v);
+    }
+
+    auto bel = g.work();
+    std::vector<int> in(g.cnt);
+    for (int i = 0; i < n; i++) {
+        for (auto j : g.adj[i]) {
+            if (bel[i] != bel[j]) {
+                in[bel[j]]++;
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < g.cnt; i++) {
+        if (i != bel[s] && in[i] == 0) {
+            ans++;
+        }
+    }
+    std::cout << ans << "\n";
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
