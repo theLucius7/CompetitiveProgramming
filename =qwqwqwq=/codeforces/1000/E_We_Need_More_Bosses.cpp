f920ecfc1904cde5bd4055aeafa233fedc4ef4b8
theLucius7 <lucius7nya@gmail.com>
2026-07-28 10:51:42 +0800

    qwq and ovo and o0o

---
+++ =qwqwqwq=/codeforces/1000/E_We_Need_More_Bosses.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+// std::set<std::pair<int, int>> E;
+struct EBCC {
+    int n;
+    std::vector<std::vector<int>> adj;
+    std::vector<int> stk;
+    std::vector<int> dfn, low, bel;
+    int cur, cnt;
+    
+    EBCC() {}
+    EBCC(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        this->n = n;
+        adj.assign(n, {});
+        dfn.assign(n, -1);
+        low.resize(n);
+        bel.assign(n, -1);
+        stk.clear();
+        cur = cnt = 0;
+    }
+    
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    void dfs(int x, int p) {
+        dfn[x] = low[x] = cur++;
+        stk.push_back(x);
+        
+        for (auto y : adj[x]) {
+            if (y == p) {
+                continue;
+            }
+            if (dfn[y] == -1) {
+                // E.emplace(x, y);
+                dfs(y, x);
+                low[x] = std::min(low[x], low[y]);
+            } else if (bel[y] == -1 && dfn[y] < dfn[x]) {
+                // E.emplace(x, y);
+                low[x] = std::min(low[x], dfn[y]);
+            }
+        }
+        
+        if (dfn[x] == low[x]) {
+            int y;
+            do {
+                y = stk.back();
+                bel[y] = cnt;
+                stk.pop_back();
+            } while (y != x);
+            cnt++;
+        }
+    }
+    
+    std::vector<int> work() {
+        dfs(0, -1);
+        return bel;
+    }
+    
+    struct Graph {
+        int n;
+        std::vector<std::pair<int, int>> edges;
+        std::vector<int> siz;
+        std::vector<int> cnte;
+    };
+    Graph compress() {
+        Graph g;
+        g.n = cnt;
+        g.siz.resize(cnt);
+        g.cnte.resize(cnt);
+        for (int i = 0; i < n; i++) {
+            g.siz[bel[i]]++;
+            for (auto j : adj[i]) {
+                if (bel[i] < bel[j]) {
+                    g.edges.emplace_back(bel[i], bel[j]);
+                } else if (i < j) {
+                    g.cnte[bel[i]]++;
+                }
+            }
+        }
+        return g;
+    }
+};
+
+void solve() {
+    int n, m;
+    std::cin >> n >> m;
+    
+    EBCC g(n);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        a--, b--;
+        g.addEdge(a, b);
+    }
+
+    g.work();
+    auto gra = g.compress();
+
+    std::vector<std::vector<int>> adj(gra.n);
+    for (auto [x, y] : gra.edges) {
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    auto bfs = [&](int s) {
+        std::queue<int> q;
+        std::vector d(n, -1);
+        d[s] = 0;
+        q.push(s);
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            for (auto y : adj[x]) {
+                if (d[y] == -1) {
+                    d[y] = d[x] + 1;
+                    q.push(y);
+                }
+            }
+        }
+        return d;
+    };
+
+    auto d0 = bfs(0);
+    int s = std::max_element(d0.begin(), d0.end()) - d0.begin();
+
+    auto ds = bfs(s);
+    int t = std::max_element(ds.begin(), ds.end()) - ds.begin();
+
+    std::cout << ds[t] << "\n";
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
